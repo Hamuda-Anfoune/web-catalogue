7f4b8fb9ebd358ec4f9cd2d83df4b66b2471721b
Hamuda <Hamuda.dev@gmail.com>
2024-07-25 08:59:10 +0100

    WebsiteController clean up, removed Tasks code

--- app/Http/Controllers/WebsiteController.php
+++ app/Http/Controllers/WebsiteController.php
@@ -43,65 +43,29 @@ class WebsiteController extends Controller
      */
     public function store(Request $request)
     {
-        // TODO: make sure that only the domain is stored and that it is unique, should do it in the request validation
-
-
-        // $request->validate([
-        //     'categories' => 'array|integer|in:categories',
-        //     'name' => 'search|string|max:255',
-        //     'name' => '|string|max:255',
-        // ]);
-
-        // $name = $request->get('name');
-
-        // $task = Task::create(['name' => $name]);
-
-        // return redirect()->route('tasks.list');
+        // TODO: make sure that only the domains are stored and that it is unique, should do it in the request validation
     }
 
-    // /**
-    //  * Update the specified resource in storage.
-    //  *
-    //  * @param  \Illuminate\Http\Request  $request
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function update(Request $request, $id)
     {
-        // $request->validate([
-        //     'done' => 'required|string',
-        // ]);
-
-        // try {
-        //     /** @var Task $task */
-        //     $task = Task::findOrFail($id);
-        //     $task->update(['done' => $request->get('done')]);
-        //     $task->save();
-
-        // } catch (\Exception $e) {
-        //     // TODO: log and/or return error message instead of of silently failing
-        // }
-
-        // return redirect()->route('tasks.list');
+        //
     }
 
-    // /**
-    //  * Remove the specified resource from storage.
-    //  *
-    //  * @param  int  $id
-    //  * @return \Illuminate\Http\Response
-    //  */
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
     public function destroy($id)
     {
-        // try {
-        //     /** @var Task $task */
-        //     $task = Task::findOrFail($id);
-        //     $task->delete();
-
-        // } catch (\Exception $e) {
-        //     // TODO: log and/or return error message instead of silently failing
-        // }
-
-        // return redirect()->route('tasks.list');
+        
     }
 }
